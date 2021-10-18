2a723684ed8078ff646394d086e55c6b2cf5fa32
Mathias Buus <mathiasbuus@gmail.com>
2021-10-18 09:16:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# hyperswarm-http-server
+
+Simply tool that proxies [http-server](https://www.npmjs.com/package/http-server) over Hyperswarm
+
+```
+npm install -g hyperswarm-http-server
+```
+
+## Usage
+
+On the computer where you want to quickly share a folder over http (over hyperswarm)
+
+```
+hyperswarm-http-server
+```
+
+Then take the command it prints and run that on the computer you want to access it on
+
+```
+hyperswarm-http-server ddafbe...
+```
+
+That's it! Just visit the localhost address it prints in a browser to access your files.
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const DHT = require('@hyperswarm/dht')
+const net = require('net')
+const pump = require('pump')
+const bind = require('bind-easy')
+
+const node = new DHT()
+
+const key = process.argv[2] && Buffer.from(process.argv[2], 'hex')
+
+if (key && key.length === 32) {
+  bind.tcp(8080).then(function (server) {
+    server.on('connection', function (socket) {
+      pump(socket, node.connect(key), socket)
+    })
+    console.log('Client mode. hyperswarm-http-server available on:')
+    console.log('  http://127.0.0.1:' + server.address().port)
+  })
+} else {
+  let port = 0
+
+  const server = node.createServer(function (socket) {
+    pump(socket, net.connect(port, 'localhost'), socket)
+  })
+
+  server.listen().then(async function () {
+    port = await freePort()
+    process.env.PORT = port
+    console.log('Server mode. To connect run this http-server on any computer')
+    console.log('  hyperswarm-http-server ' + server.address().publicKey.toString('hex'))
+    console.log()
+    console.log('Output from http-server:')
+    console.log()
+    require('http-server/bin/http-server')
+  })
+}
+
+function freePort () {
+  return new Promise((resolve) => {
+    const server = net.createServer()
+    server.listen(0)
+    server.on('listening', function () {
+      const { port } = server.address()
+      server.close()
+      server.on('close', function () {
+        resolve(port)
+      })
+    })
+  })
+}
